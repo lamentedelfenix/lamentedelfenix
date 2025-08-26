bc2b76310297046dfcaf7822a3b343fd22be19f6
lamentedelfenix <lamentedelfenix@gmail.com>
2025-08-26 19:05:11 +0200

    Update videos.js

--- videos.js
+++ videos.js
@@ -3,21 +3,21 @@ const videosData = [
     category: "Inspiración",
     title: 'Ilia Topuria (Campeón del Mundo): "Deposito mi Confianza en Dios"',
     description: "Ilia Topuria antes de ser campeón indiscutido y contra todo pronóstico de MMA; habla de que aplica uno de los principios fundamentales de UCDM. Depositar tu fe en Dios y que lo demás se dará.",
-    url: "https://youtu.be/watch?v=c-OTeDTThQIQ",
+    url: "https://www.youtube.com/watch?v=c-OTeDTThQIQ",
     thumbnail: ""
   },
   {
     category: "Inspiración",
     title: "Jim Carrey sobre decidir entre Amor o Miedo",
     description: "Discurso de Graduación en la Universidad Internacional Maharishi en la que Jim Carrey habla sobre decidir entre Amor y Miedo.",
-    url: "https://youtu.be/VAFTo7gTa-g?si=ShcfWqDLejsbMW2h",
+    url: "https://www.youtube.com/watch?v=VAFTo7gTa-g",
     thumbnail: ""
   },
   {
     category: "Inspiración",
     title: "Entrevista a empresaria Marta Marcilla - La importancia de la mentalidad para el éxito.",
     description: "Entrevista a Mujer Multi-millonaria, Marta Marcilla habla sobre de su vida y de cómo se hizo millonaria cambiando de mentalidad y su autoconcepto.",
-    url: "https://youtu.be/eEaI-QKNw7o?si=VhcQtbDj_k01t_MDW",
+    url: "https://www.youtube.com/watch?v=eEaI-QKNw7o",
     thumbnail: ""
   },
   {
@@ -45,35 +45,35 @@ const videosData = [
     category: "Conocimiento",
     title: "Entrevista a Enrique Jurado, maestro de Coaching y Programación Neurolingüística.",
     description: "El maestro de Darío Arrigazzi en PNL y Coaching, Enrique Jurado habla sobre experiencias personales y la importancia de cómo te hablas a ti mismo, entre otras cosas interesantes.",
-    url: "https://youtu.be/9_GsoU0d-eA?si=g73-yqVCV6oDNGBg",
+    url: "https://www.youtube.com/watch?v=9_GsoU0d-eA",
     thumbnail: ""
   },
   {
     category: "Audio",
     title: "Mini Podcast Sobre la Mente de Cristo vs Ego",
     description: "Conversación sobre los textos de Un Curso de Milagros.",
-    url: "https://youtu.be/PYrZUTZ4Gus",
+    url: "https://www.youtube.com/watch?v=PYrZUTZ4Gus",
     thumbnail: ""
   },
   {
     category: "Conocimiento",
     title: "Entrevista a Ramón Campayo (194 IQ)",
     description: "El memorizador más rápido de la historia y ostenta numerosos récords internacionales en memoria. Autor de varios best seller y creador del sistema 'Speed Memory', referente en entrenamiento y competición de alto rendimiento mental. Aborda cómo crear la realidad desde el pensamiento, el papel esencial del subconsciente en nuestras decisiones, y su convicción de que la muerte no es un final, sino una transición.",
-    url: "https://youtu.be/A8bczOHgpmY",
+    url: "https://www.youtube.com/watch?v=A8bczOHgpmY",
     thumbnail: ""
   },
   {
     category: "Película",
     title: "El Cambio - Wayne Dyer",
     description: "Película que explora el viaje espiritual de la segunda mitad de la vida, donde se busca un propósito y significado más allá de la ambición y el logro material, enfocándose en la contribución única y personal al mundo y la búsqueda de la divinidad en el presente.",
-    url: "https://youtu.be/hpFn6q_FmSQ",
+    url: "https://www.youtube.com/watch?v=hpFn6q_FmSQ",
     thumbnail: ""
   },
   {
     category: "Conocimiento",
     title: "Entrevista a Experta en visión Extra-Ocular",
     description: "Camila demuestra como entrenando la mente se puede ver sin usar lo ojos y lo demuestra en directo.",
-    url: "https://youtu.be/uYhCcl7Anr4",
+    url: "https://www.youtube.com/watch?v=uYhCcl7Anr4",
     thumbnail: ""
   }
 ];
